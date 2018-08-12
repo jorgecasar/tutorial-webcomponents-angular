6bc76a1d200cedd29955766a11e1e815e29d4274
Jorge del Casar <jorge.casar@gmail.com>
2018-08-12 18:56:40 +0200

    chore(app): load polyfills

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,27 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
+declare global {
+  interface Window {
+    WebComponents: {
+      ready: boolean;
+    };
+  }
+}
+
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.log(err));
+function bootstrapModule() {
+  platformBrowserDynamic().bootstrapModule(AppModule)
+    .catch(err => console.log(err));
+}
+
+if (window.WebComponents.ready) {
+  // Web Components are ready
+  bootstrapModule();
+} else {
+  // Wait for polyfills to load
+  window.addEventListener('WebComponentsReady', bootstrapModule);
+}
