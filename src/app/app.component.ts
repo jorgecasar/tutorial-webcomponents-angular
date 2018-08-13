ff75f509abd847ee19a30b61e1c96291f4fbd8bf
Jorge del Casar <jorge.casar@gmail.com>
2018-08-13 11:24:27 +0200

    feat(wc-mood): set property to Web Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,15 @@ import './wc-mood/wc-mood';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tutorial-webcomponents-angular';
+  private moods: Array<string> = ['awesome', 'formidable', 'great', 'terrifying', 'wonderful', 'astonishing', 'breathtaking'];
+  mood: string;
+
+  constructor() {
+    this.randomMood();
+  }
+
+  randomMood() {
+    const index = Math.floor(Math.random()*this.moods.length);
+    this.mood = this.moods[index];
+  }
 }
